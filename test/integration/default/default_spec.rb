fe6d0dd7983d9a0006565641cc6f8ca1fbbecbfb
rizkidoank <rizki@rizkidoank.com>
2017-10-31 11:09:00 +0700

    add spec for apt_sources_list

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -1,3 +1,7 @@
 describe file('/etc/apt/sources.list') do
-  it { should be_installed}
+  it { should exist }
+  it { should be_file }
+  its('owner') { should eq 'root' }
+  its('group') { should eq 'root' }
+  its('mode') { should cmp '00644' }
 end
